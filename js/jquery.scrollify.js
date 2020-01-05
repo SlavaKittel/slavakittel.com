4b2cd38aa7a5fdac474735cc25da5c4f9b4c41e4
Slava <49091983+SlavaKittel@users.noreply.github.com>
2020-01-05 13:59:59 +0200

    1

--- js/jquery.scrollify.js
+++ js/jquery.scrollify.js
@@ -79,7 +79,7 @@ if touchScroll is false - update index
       interstitialSection: "",
       easing: "easeOutExpo",
       scrollSpeed: 800,
-      offset: 200,
+      offset: 10,
       scrollbars: true,
       target: "html,body",
       standardScrollElements: false,
